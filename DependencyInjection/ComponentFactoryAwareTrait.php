5a5cb23bd4fb32c4ac70252fa0d996888cdcfefe
Ivan Vegner <vegner@slrealty.ru>
2017-08-18 15:06:46 +0300

    Use auto declare

--- DependencyInjection/ComponentFactoryAwareTrait.php
+++ DependencyInjection/ComponentFactoryAwareTrait.php
@@ -4,9 +4,14 @@ namespace SD\ComponentCore\DependencyInjection;
 use SD\ComponentCore\Factory\ComponentFactoryInterface;
 
 trait ComponentFactoryAwareTrait {
+    private $autoDeclareComponentFactory = 'componentFactory';
     private $componentFactory;
 
     public function setComponentFactory(ComponentFactoryInterface $componentFactory) {
         $this->componentFactory = $componentFactory;
+    }
+
+    public function getComponentFactory(): ComponentFactoryInterface {
+        return $this->componentFactory;
     }
 }
