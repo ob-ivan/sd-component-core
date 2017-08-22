249248aaddd31fe299e3f9d02b4ec5d7141a1578
Ivan Vegner <vegner@slrealty.ru>
2017-08-22 13:03:17 +0300

    Add namespaces parameter to provider

--- src/DependencyInjection/ComponentFactoryProvider.php
+++ src/DependencyInjection/ComponentFactoryProvider.php
@@ -5,11 +5,17 @@ use SD\ComponentCore\Factory\ComponentFactory;
 use SD\DependencyInjection\ProviderInterface;
 
 class ComponentFactoryProvider implements ProviderInterface {
+    private $namespaces;
+
+    public function __construct(array $namespaces = []) {
+        $this->namespaces = array_map('strval', $namespaces);
+    }
+
     public function getServiceName(): string {
         return 'componentFactory';
     }
 
     public function provide() {
-        return new ComponentFactory();
+        return new ComponentFactory($this->namespaces);
     }
 }
