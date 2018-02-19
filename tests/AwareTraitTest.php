3c5b4e92d31434dd0f16f532f5847ab57b276ef1
Ivan Vegner <vegner@slrealty.ru>
2018-02-19 13:10:44 +0300

    Fix deprecated container constructor call

--- tests/AwareTraitTest.php
+++ tests/AwareTraitTest.php
@@ -7,7 +7,7 @@ use SD\DependencyInjection\Container;
 
 class AwareTraitTest extends TestCase {
     public function testInheritAutoDeclare() {
-        $container = new Container([], 'container');
+        $container = new Container();
         $container->connect(new ComponentFactoryProvider(['tests']));
         $expectedComponentFactory = $container->get('componentFactory');
         $data = $expectedComponentFactory->create('SubclassComponent')->getData();
